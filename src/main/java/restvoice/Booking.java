9b6619cd886986acf55c7f6901e0a30d0c599623
Ralf Wirdemann <ralf.wirdemann@gmail.com>
2018-06-06 14:48:04 +0200

    Add new booking view

---
+++ src/main/java/restvoice/Booking.java
@@ -0,0 +1,22 @@
+package restvoice;
+
+public class Booking {
+    private String description;
+    private float hours;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public float getHours() {
+        return hours;
+    }
+
+    public void setHours(float hours) {
+        this.hours = hours;
+    }
+}
--- src/main/java/restvoice/HomeController.java
+++ src/main/java/restvoice/HomeController.java
@@ -3,6 +3,7 @@ package restvoice;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.ServletException;
@@ -22,6 +23,21 @@ public class HomeController {
 
     @GetMapping("/new_invoice")
     public String newInvoice() {
+        return "invoice";
+    }
+
+    @GetMapping("/booking")
+    public String newBooking(Model model) {
+        model.addAttribute("booking", new Booking());
+        return "booking";
+    }
+
+    @GetMapping("/invoice/{id}")
+    public String invoice(@PathVariable int id, Model model) {
+        RestTemplate restTemplate = new RestTemplate();
+        Invoice response = restTemplate.getForObject("http://localhost:8190/invoice/" + id, Invoice.class);
+        model.addAttribute("invoice", response);
+
         return "invoice";
     }
 
